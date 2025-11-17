3443ea9aa0ab25e7f88a47234ab47b8c41cafbbd
matias9205 <matiasmazparrote@hotmail.com>
2025-11-17 15:17:17 +0000

    create new ELT process using DataBricks

---
+++ databricks/SQL/analytics.sql
@@ -0,0 +1,13 @@
+SELECT
+  *
+FROM
+  formula_1.stg_laps_by_drivers_sessions_meetings
+WHERE
+  country_name ='Brazil';
+
+SELECT
+  *
+FROM
+  formula_1.stg_laps_by_drivers_sessions_meetings
+WHERE
+  lap_duration IS NULL
---
+++ databricks/SQL/create_staging_formula1_compiled_tables_cars_and_laps.sql
@@ -0,0 +1,75 @@
+CREATE OR REPLACE TABLE formula_1.STG_laps_by_drivers_sessions_meetings AS 
+SELECT
+	meet.meeting_name,
+	meet.meeting_official_name,
+	meet.meeting_code,
+	sess.session_name,
+	sess.session_type,
+	meet.circuit_short_name,
+	meet.country_code AS meeting_country_code,
+	meet.country_name,
+	meet.location,
+	meet.date_start as meeting_date_start,
+	sess.date_start as session_date_start,
+	sess.date_end as session_date_end,
+	sess.gmt_offset,
+	driv.first_name,
+	driv.last_name,
+	driv.full_name,
+	driv.broadcast_name,
+	driv.country_code AS driver_country_code,
+	driv.team_name,
+	lap.lap_number,
+	lap.date_start as lap_date_start,
+	CAST(lap.duration_sector_1 AS DECIMAL(8,2)) AS duration_sector_1,
+	CAST(lap.duration_sector_2 AS DECIMAL(8,2)) AS duration_sector_2,
+	CAST(lap.duration_sector_3 AS DECIMAL(8,2)) AS duration_sector_3,
+	CAST(lap.i1_speed AS DECIMAL(8,2)) AS i1_speed,
+	CAST(lap.i2_speed AS DECIMAL(8,2)) AS i2_speed,
+	lap.is_pit_out_lap,
+	lap.lap_duration,
+	lap.segments_sector_1,
+	lap.segments_sector_2,
+	lap.segments_sector_3,
+	CAST(lap.st_speed AS DECIMAL(8,2)) AS st_speed
+FROM
+	formula_1.raw_meetings AS meet
+JOIN formula_1.raw_sessions AS sess ON meet.meeting_key = sess.meeting_key
+JOIN formula_1.raw_drivers AS driv ON meet.meeting_key = driv.meeting_key AND sess.session_key = driv.session_key
+JOIN formula_1.raw_laps AS lap ON meet.meeting_key = lap.meeting_key AND sess.session_key = lap.session_key AND driv.driver_number = lap.driver_number;
+
+
+--CREACION DE TABLA DE DATOS CRUDOS UNIFICADOS (MEETINGS+SESSIONS+DRIVERS+CARS)
+CREATE OR REPLACE TABLE formula_1.STG_cars_by_drivers_sessions_meetings AS
+SELECT
+	meet.meeting_name,
+	meet.meeting_official_name,
+	meet.meeting_code,
+	sess.session_name,
+	sess.session_type,
+	meet.circuit_short_name,
+	meet.country_code AS meeting_country_code,
+	meet.country_name,
+	meet.location,
+	meet.date_start as meeting_date_start,
+	sess.date_start as session_date_start,
+	sess.date_end as session_date_end,
+	sess.gmt_offset,
+	driv.first_name,
+	driv.last_name,
+	driv.full_name,
+	driv.broadcast_name,
+	driv.country_code AS driver_country_code,
+	driv.team_name,
+	car.date,
+	car.brake,
+	car.drs,
+	car.n_gear,
+	car.rpm,
+	car.throttle,
+	car.speed
+FROM
+	formula_1.raw_meetings AS meet
+JOIN formula_1.raw_sessions AS sess ON meet.meeting_key = sess.meeting_key
+JOIN formula_1.raw_drivers AS driv ON meet.meeting_key = driv.meeting_key AND sess.session_key = driv.session_key
+JOIN formula_1.raw_cars_data AS car ON meet.meeting_key = car.meeting_key AND sess.session_key = car.session_key AND driv.driver_number = car.driver_number;
