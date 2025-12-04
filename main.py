27aeff8be6c1126c9fe07f4ae133976e5ff5f00b
matias9205 <matiasmazparrote@hotmail.com>
2025-12-04 10:36:39 -0300

    add error logs saving after run API fetching

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from datetime import datetime
-import os
 import sys
-import requests
 import pandas as pd
-import time
 import logging
 
 from src.config.settings import Settings
@@ -19,8 +16,12 @@ logging.basicConfig(
 
 class Main:
     def __init__(self):
-        self.year = [elem for elem in sys.argv[1:] if elem.isdigit() and len(elem) == 4][0]
-        self.month = [elem for elem in sys.argv[1:] if elem.isdigit() and len(elem) <= 2][0]
+        self.year = [
+            elem for elem in sys.argv[1:] if elem.isdigit() and len(elem) == 4
+        ][0]
+        self.month = [
+            elem for elem in sys.argv[1:] if elem.isdigit() and len(elem) <= 2
+        ][0]
         self.settings = Settings()
         self.helpers = Helpers()
         self.session_endpoint = self.settings.SESSIONS_ENDPOINT
@@ -28,7 +29,9 @@ class Main:
         self.drivers_endpoint = self.settings.DRIVERS_ENDPOINT
         self.cars_endpoint = self.settings.CARS_ENDPOINT
         self.laps_endpoint = self.settings.LAPS_ENDPOINT
-        self.min_max_date = self.helpers.get_max_date_of_month(int(self.year), int(self.month))
+        self.min_max_date = self.helpers.get_max_date_of_month(
+            int(self.year), int(self.month)
+        )
         self.min_date = self.min_max_date[0]
         self.max_date = self.min_max_date[1]
         self.extract = Extract()
@@ -52,7 +55,9 @@ class Main:
             )
             if not self.sessions.empty:
                 logging.info(self.sessions.info())
-                self.load.load_data_in_DB(self.sessions, "sessions", self.year, self.month)
+                self.load.load_data_in_DB(
+                    self.sessions, "sessions", self.year, self.month
+                )
                 logging.info("")
                 logging.info("")
                 logging.info("")
@@ -60,34 +65,48 @@ class Main:
                     "-----------------------------------------meetings-----------------------------------------"
                 )
                 self.meetings = self.extract.extract_data(
-                    self.meetings_endpoint.format(self.min_date, self.max_date, self.year)
+                    self.meetings_endpoint.format(
+                        self.min_date, self.max_date, self.year
+                    )
                 )
                 if not self.meetings.empty:
                     logging.info(self.meetings.info())
-                self.load.load_data_in_DB(self.meetings, "meetings", self.year, self.month)
+                self.load.load_data_in_DB(
+                    self.meetings, "meetings", self.year, self.month
+                )
                 logging.info("")
                 logging.info("")
                 logging.info("")
                 logging.info(
                     "-----------------------------------------drivers-----------------------------------------"
                 )
-                self.drivers = self.extract.extract_data(self.drivers_endpoint, self.sessions)
+                self.drivers = self.extract.extract_data(
+                    self.drivers_endpoint, self.sessions
+                )
                 if not self.drivers.empty:
                     logging.info(self.drivers.info())
-                self.load.load_data_in_DB(self.drivers, "drivers", self.year, self.month)
+                self.load.load_data_in_DB(
+                    self.drivers, "drivers", self.year, self.month
+                )
                 logging.info("")
                 logging.info("")
                 logging.info("")
             else:
-                logging.error(f"THERE ARE NO SESSIONS AVAILABLES FOR PERIOD {self.year}-{self.month}")
+                logging.error(
+                    f"THERE ARE NO SESSIONS AVAILABLES FOR PERIOD {self.year}-{self.month}"
+                )
             if not self.drivers.empty:
                 logging.info(
                     "-----------------------------------------cars data-----------------------------------------"
                 )
-                self.cars_data = self.extract.extract_data(self.cars_endpoint, self.drivers)
+                self.cars_data = self.extract.extract_data(
+                    self.cars_endpoint, self.drivers
+                )
                 if not self.cars_data.empty:
                     logging.info(self.cars_data.info())
-                self.load.load_data_in_DB(self.cars_data, "cars_data", self.year, self.month)
+                self.load.load_data_in_DB(
+                    self.cars_data, "cars_data", self.year, self.month
+                )
                 logging.info("")
                 logging.info("")
                 logging.info("")
@@ -101,13 +120,13 @@ class Main:
                 logging.info("")
                 logging.info("")
                 logging.info("")
-            # df_error_logs = pd.DataFrame(self.extract.error_logs)
-            # logging.info(f"LOS ERROR LOG TIENEN {df_error_logs.shape[0]} FILAS")
-            # self.settings.create_new_dir(["src", "logs", "csv"])
-            # file_name = f'error_log_{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.csv'
-            # output_dir_file = self.settings.get_file_path(["src", "logs", "csv"], file_name)
-            # if not df_error_logs.empty:
-            #     df_error_logs.to_csv(output_dir_file.replace(" ", "_"), encoding='utf-8-sig', index=False)
+            df_error_logs = self.extract.create_error_log_table()
+            logging.info(f"LOS ERROR LOG TIENEN {df_error_logs.shape[0]} FILAS")
+            self.load.load_csv(
+                df_error_logs,
+                "error_logs",
+                __timestamp__=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            )
 
 
 if __name__ == "__main__":
