78caade89d915fa8eb8ce80fcd07b2ca08882ca9
matiasfeliu92 <matumazparrote@gmail.com>
2025-12-09 10:51:37 -0300

    add PostgreSQL connection string

--- src/config/db.py
+++ src/config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 import pyodbc
+import psycopg2
 
 from src.config.settings import Settings
 
@@ -8,17 +9,8 @@ from src.config.settings import Settings
 class ManageDB:
     def __init__(self):
         settings = Settings()
-        self.conn_string = (
-            f"mssql+pyodbc://@{settings.SQL_SERVER_HOST}/{settings.SQL_SERVER_DB_USE}"
-            "?driver=ODBC+Driver+17+for+SQL+Server"
-            "&Trusted_Connection=yes"
-        )
-        self.conn_string_for_cursor = (
-            f"DRIVER={{ODBC Driver 17 for SQL Server}};"
-            f"SERVER={settings.SQL_SERVER_HOST};"
-            f"DATABASE={settings.SQL_SERVER_DB};"
-            "Trusted_Connection=yes;"
-        )
+        self.conn_string = settings.POSTGRES_CONNECTION_STRING
+        self.conn_string_for_cursor = settings.POSTGRES_CURSOR_CONNECTION_STRING
         self.engine = None
 
     def create_engine(self):
@@ -31,10 +23,18 @@ class ManageDB:
             print("THERE WAS AN ERROR WITH CONNECTION \n", str(e))
 
     def create_connection(self):
-        try:
-            conn = pyodbc.connect(self.conn_string_for_cursor, autocommit=True)
-            print("Conexión exitosa")
-            return conn
-        except pyodbc.Error as ex:
-            sqlstate = ex.args[0]
-            print(f"Error al conectar: {sqlstate}")
+        if "postgresql" in self.conn_string:
+            try:
+                conn = psycopg2.connect(self.conn_string_for_cursor)
+                print("CONNECTION ESTABISH SUCCESSFULLY")
+                return conn
+            except psycopg2.Error as ex:
+                print(f"Error al conectar: {ex}")
+        else:
+            try:
+                conn = pyodbc.connect(self.conn_string_for_cursor, autocommit=True)
+                print("Conexión exitosa")
+                return conn
+            except pyodbc.Error as ex:
+                sqlstate = ex.args[0]
+                print(f"Error al conectar: {sqlstate}")
